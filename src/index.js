4822d2e11079bfe0167d43b1852efe8604421bfe
Khell <git@khell.org>
2017-05-06 21:00:00 +1000

    Initial commit at 1.0.0.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const less = require('less');
+
+const defaultOptions = {
+    nameProjectionFunc: null
+};
+
+module.exports = (str = '', options = defaultOptions) => {
+    Object.assign({}, defaultOptions, options);
+    return new Promise((resolve, reject) => {
+        less.parse(str, options.config, (err, root, imports, lessOpts) => {
+            if (err)
+                reject(err);
+
+            let evalEnv = new less.contexts.Eval(lessOpts);
+            let evaldRoot = root.eval(evalEnv);
+            let ruleset = evaldRoot.rules;
+
+            resolve(ruleset
+                        .filter((node) => node.variable === true)
+                        .reduce((prev, curr) => {
+                            let entry = {};
+                            entry[typeof(options.nameProjectionFunc) === "function" ? options.nameProjectionFunc(curr.name) : curr.name] = curr.value.toCSS(lessOpts);
+                            return Object.assign({}, prev, entry);
+                        }, {})
+            );
+        });
+    });
+};
---
+++ test/test.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const lessVariablesToJson = require('../src');
+
+chai.use(chaiAsPromised);
+const should = chai.should();
+
+describe('less-variables-to-json', function() {
+    it('should return JSON with no properties if given no arguments', function() {
+        return lessVariablesToJson().should.eventually.deep.equal({});
+    });
+
+    it('should return JSON with correctly mapped properties, given correctly formed LESS variables', function() {
+        let lessStr = "@myvar: 1234; @mycolor: #fff;";
+        let expectedJSON = {
+            "@myvar": "1234",
+            "@mycolor": "#fff"
+        };
+
+        return lessVariablesToJson(lessStr).should.eventually.deep.equal(expectedJSON);
+    });
+
+    it('should return JSON with mutated names, as directed by the projection function', function() {
+        let lessStr = "@myvar: 1234; @mycolor: #fff;";
+        let expectedJSON = {
+            "MYVAR": "1234",
+            "MYCOLOR": "#fff"
+        };
+        let nameProjectionFunc = (str) => str.substr(1).toUpperCase();
+
+        return lessVariablesToJson(lessStr, { nameProjectionFunc }).should.eventually.deep.equal(expectedJSON);
+    });
+
+    it('should return JSON with correctly mapped properties and LESS selectors removed', function() {
+        let lessStr = "@myvar: 1234; @mycolor: #fff; .myclass { color: #000; }";
+        let expectedJSON = {
+            "@myvar": "1234",
+            "@mycolor": "#fff"
+        };
+
+        return lessVariablesToJson(lessStr).should.eventually.deep.equal(expectedJSON);
+    });
+
+    it('should return JSON with only valid LESS transformed and invalid LESS stripped', function() {
+        let lessStr = "@myvar???:xsas 1234 @mycolor.#fff; @validvar: 60;";
+        let expectedJSON = {
+            "@validvar": "60"
+        };
+
+        return lessVariablesToJson(lessStr).should.eventually.deep.equal(expectedJSON);
+    });
+
+    it('should error if given malformed LESS with no terminating semicolon', function() {
+        let lessStr = "@myvar: 1234; @mycolor: #fff";
+        return lessVariablesToJson(lessStr).should.eventually.be.rejected;
+    });
+})
